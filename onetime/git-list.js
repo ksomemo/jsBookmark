8bfb396f222193ed89ae9010b2464ee73becf0e3
k_somemo <kntsmy@hotmail.co.jp>
2013-11-03 06:14:22 +0900

    gistのリポジトリをcloneするコマンド一覧を作成するJavaScriptを作成した。

---
+++ onetime/git-list.js
@@ -0,0 +1,31 @@
+var user = "" || window.prompt("Input gist username.");
+var baseUrl = "https://gist.github.com/";
+var page = 1;
+var cloneCommand = "";
+
+function gistCloneCommandList() {
+	$.ajax({
+		url: baseUrl + user + "?page=" + page,
+		success: function (data) {
+			var contents = $(data);
+			contents.find("div.gist.gist-item span.creator a[href^='/" + user + "/']").each(function () {
+				cloneCommand += "git clone " + this.href + " " + this.children[0].innerHTML + "\n";
+			});
+
+			if (contents.find("div.pagination span.disabled:contains('Older')").length > 0) {
+				alert("finish extract clone command.");
+				console.log(cloneCommand);
+				return;
+			}
+
+			page++;
+			setTimeout(gistCloneCommandList, 200);
+		},
+		error: function () {
+			alert(user + " does not exist.");
+		}
+	});
+}
+
+gistCloneCommandList();
+
